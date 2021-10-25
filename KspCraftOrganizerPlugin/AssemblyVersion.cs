758f1a8444607195d881e2360c80a03a00571cda
Jonathan Bayer <linuxgurugamer@gmail.com>
2021-10-25 10:32:38 -0400

    Updated csproj for new directory names

--- KspCraftOrganizerPlugin/AssemblyVersion.cs
+++ KspCraftOrganizerPlugin/AssemblyVersion.cs
@@ -5,5 +5,5 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("0.0.0.0")]
- [assembly: AssemblyFileVersion("0.0.0.0")]
+ [assembly: AssemblyVersion("1.7.0.0")]
+ [assembly: AssemblyFileVersion("1.7.0.0")]
